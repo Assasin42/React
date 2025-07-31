481d55aabf542a635b274beebde2f599bf614595
Ali <gojosensei963@hotmail.com>
2025-07-31 17:50:35 +0300

    boarddaki kartların düzenlenmesi

--- src/Kartlar.tsx
+++ src/Kartlar.tsx
@@ -1,9 +1,7 @@
 import React, { use, useEffect, useState } from 'react';
 import { Card, Col, Row ,Space } from 'antd';
 import { IKart } from './MyMenu';
-
-
-
+import './Kartlar.css';
 
 // ...existing code...
 type KartlarProps = {
@@ -17,19 +15,35 @@ const Kartlar: React.FC<KartlarProps> = ({ data }) => {
 
   return (
     <>
-      <Row gutter={16}>
-        <Col span={24}>
-        <Space direction="horizontal" size="middle" style={{ display: 'flex', justifyContent: 'left', alignItems: 'center' }}>
-          {data.map((kart, index) => (
-            <Card key={index} title={kart.type}>
+    <h3 className='h3'>Talepler</h3>
+    <hr />
+      <Row gutter={16} >
+        <Col span={4}>
+        
+        <h3 className='h3'>İlk kolon</h3>
+        <Space className='space-style'  direction="vertical" size="middle" >
+          {data.filter((kart) => kart.type === 'proje').map((kart, index) => (
+          <Card key={index} title={kart.type} style={{ width: 180, height: 213 }}>
               <p>Proje Adı: {kart.projectName}</p>
               <p>Proje Amacı: {kart.projectGoal}</p>
             </Card>
           ))}
         </Space>
         </Col>
+      
+        <Col span={4}>
+        <h3 className='h3'>İkinci kolon</h3>
+        <Space className="space-style" direction="vertical" size="middle" >
+        {data.filter((kart) => kart.type === 'görev').map((kart, index) => (
+          <Card key={index} title={kart.type} style={{ width: 180, height: 213 }}>
+              <p>Proje Adı: {kart.projectName}</p>
+              <p>Proje Amacı: {kart.projectGoal}</p>
+              
+            </Card>
+          ))}    
+        </Space>
+        </Col>
       </Row>
-    
     </>
   );
 };
